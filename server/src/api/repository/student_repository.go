3fa938e1ad989edeeb5be60978b1735283c0e309
enomoto11 <t.enomoto@catallaxy.me>
2023-06-04 02:40:30 +0900

    repositoryに必要な関数を粗方用意

--- server/src/api/repository/student_repository.go
+++ server/src/api/repository/student_repository.go
@@ -3,11 +3,18 @@ package repository
 import (
 	"context"
 	"hellovis/api/model"
+	"hellovis/api/utils"
 	"hellovis/ent"
+	"hellovis/ent/student"
+
+	"github.com/google/uuid"
 )
 
 type StudentRepository interface {
-	Create(ctx context.Context, s *model.Student) (*model.Student, error)
+	Create(ctx *context.Context, s *model.Student) (*model.Student, error)
+	FindByID(ctx *context.Context, id uuid.UUID) (*model.Student, error)
+	FindByManavisCode(ctx *context.Context, manavisCode string) (*model.Student, error)
+	FindAllByGradeAndIsInHigh(ctx *context.Context, grade int, isInHigh bool) ([]*model.Student, error)
 }
 
 type studentRepository struct {
@@ -18,7 +25,7 @@ func NewStudentRepository(client *ent.Client) StudentRepository {
 	return &studentRepository{client}
 }
 
-func (sr *studentRepository) Create(ctx context.Context, s *model.Student) (*model.Student, error) {
+func (sr *studentRepository) Create(ctx *context.Context, s *model.Student) (*model.Student, error) {
 	entity, err := sr.client.Student.Create().
 		SetID(s.GetID()).
 		SetFirstName(s.GetFirstName()).
@@ -26,13 +33,47 @@ func (sr *studentRepository) Create(ctx context.Context, s *model.Student) (*mod
 		SetGrade(s.GetGrade()).
 		SetManavisCode(s.GetManavisCode()).
 		SetIsHighSchool(s.GetIsInHighSchool()).
-		Save(ctx)
+		Save(*ctx)
 
 	if err != nil {
 		return nil, err
 	}
 
 	return newStudentFromEntity(entity)
+}
+
+func (sr *studentRepository) FindByID(ctx *context.Context, id uuid.UUID) (*model.Student, error) {
+	entity, err := sr.client.Student.Query().
+		Where(student.IDEQ(id)).
+		Only(*ctx)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return newStudentFromEntity(entity)
+}
+
+func (sr *studentRepository) FindByManavisCode(ctx *context.Context, manavisCode string) (*model.Student, error) {
+	entity, err := sr.client.Student.Query().
+		Where(student.ManavisCodeEQ(manavisCode)).
+		Only(*ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return newStudentFromEntity(entity)
+}
+
+func (sr *studentRepository) FindAllByGradeAndIsInHigh(ctx *context.Context, grade int, isInHigh bool) ([]*model.Student, error) {
+	entities, err := sr.client.Student.Query().
+		Where(student.GradeEQ(grade), student.IsHighSchoolEQ(isInHigh)).
+		All(*ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return utils.MapSliceWithError(entities, newStudentFromEntity)
 }
 
 func newStudentFromEntity(entity *ent.Student) (*model.Student, error) {
