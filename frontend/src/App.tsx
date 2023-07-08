89304bc15e2c16ee29e1c47410aca6c47322a2af
enomoto11 <t.enomoto@catallaxy.me>
2023-07-08 19:45:06 +0900

    auth0での認証を追加

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useAuth0 } from "@auth0/auth0-react";
 
 function App() {
+  const { isAuthenticated, loginWithPopup, logout } = useAuth0();
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {!isAuthenticated ? (
+          <button onClick={() => loginWithPopup()}>ログイン</button>
+        ) : (
+          <button
+            onClick={() => {
+              console.log("未実装");
+              logout({});
+            }}
+          >
+            Log out
+          </button>
+        )}
       </header>
     </div>
   );
