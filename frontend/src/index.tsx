0643c70ab6dcda6cb6f4a4153e4fb3ba7ed6cba7
enomoto11 <t.enomoto@catallaxy.me>
2023-07-09 02:09:44 +0900

    余計な記述を削除

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,10 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import { Auth0Provider } from "@auth0/auth0-react";
-import { DOMAIN, CLIENT_ID } from "./env/auth0.env";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { Auth0Provider } from '@auth0/auth0-react';
+import { DOMAIN, CLIENT_ID } from './env/auth0.env';
 
 ReactDOM.render(
   <Auth0Provider
@@ -16,7 +15,5 @@ ReactDOM.render(
       <App />
     </React.StrictMode>
   </Auth0Provider>,
-  document.getElementById("root")
+  document.getElementById('root'),
 );
-
-reportWebVitals();
